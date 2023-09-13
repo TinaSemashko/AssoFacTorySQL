a0fe6e7d6a9039a671f5696490d7b284874c4f74
harry <harryljsp@gmail.com>
2023-09-13 12:50:17 +0200

    footer

--- packages/client/src/footer/footer.jsx
+++ packages/client/src/footer/footer.jsx
@@ -1,7 +1,44 @@
 import * as S from "./footer.styled";
 
+
 const Footer = () => {
-  return <S.MainContainer>Footer</S.MainContainer>;
+  
+  return (
+    <S.MainContainer>
+      <S.FooterContent>
+        <S.FooterLogo>
+          {/* Inclure ici le logo ou le texte du footer */}
+          
+        </S.FooterLogo>
+        <S.FooterLinks>
+   <ul>
+    <a href="/">Nous Contacter:</a>
+  <li>
+    <a href="/">
+    <img src="Ringing Phone.png" alt="" /> Téléphone :
+    </a>
+  </li>
+  <li>
+    <a href="/">E-Mail :</a>
+  </li>
+  <li>
+    <a href="/">Adresse :</a>
+  </li>
+  </ul>
+        </S.FooterLinks>
+        <S.FooterSocialMedia>
+          {/* Ajoutez ici des icônes ou des liens vers les réseaux sociaux */}
+          
+          {/* Ajoutez d'autres icônes ou liens sociaux ici */}
+        </S.FooterSocialMedia>
+      </S.FooterContent>
+      <S.FooterCopyright>
+        {/* Ajoutez ici le texte de copyright */}
+        © 2023 AssoFacTory. Tous Droits Réservés.
+      </S.FooterCopyright>
+    </S.MainContainer>
+  );
 };
 
 export default Footer;
+
