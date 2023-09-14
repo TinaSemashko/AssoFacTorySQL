1ec27e2345eb906c7b76a5632b3dc31497ca859c
Kaiju971 <129183625+Kaiju971@users.noreply.github.com>
2023-09-14 11:33:03 +0200

    Route

--- packages/client/src/app/appRoutes.js
+++ packages/client/src/app/appRoutes.js
@@ -6,10 +6,14 @@ import NotFound from "../pages/notFound/notFound";
 const Home = lazy(() => import("../pages/home/accueil"));
 const Blog = lazy(() => import("../pages/blog/blog"));
 <<<<<<< HEAD
+<<<<<<< HEAD
 const Inscription = lazy(() => import("../pages/inscription/inscription"));
 =======
 const Inscription = lazy(() => import("../pages/inscription/inscription.jsx"));
 >>>>>>> 70145443 (install inscription)
+=======
+const Inscription = lazy(() => import("../pages/inscription/inscription"));
+>>>>>>> 1044be11 (Route)
 
 export const AppRoutes = () => {
   return (
