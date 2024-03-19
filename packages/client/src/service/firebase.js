5d90d72725294cd008144f85d333b8c13902e484
TinaSemashko <akateria.satory@gmail.com>
2024-03-19 21:22:10 +0100

    firebse

--- packages/client/src/service/firebase.js
+++ packages/client/src/service/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCNgEzCAJlnlRHVoi21OXJgzWl13wqKSCg",
-  authDomain: "assofactory-b7f0b.firebaseapp.com",
-  projectId: "assofactory-b7f0b",
-  storageBucket: "assofactory-b7f0b.appspot.com",
-  messagingSenderId: "528163036489",
-  appId: "1:528163036489:web:a0af3a6f172a6d8c6bce98",
-  measurementId: "G-JKG8WC2ZV7",
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
